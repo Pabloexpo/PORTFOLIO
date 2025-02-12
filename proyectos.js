27069a9fcae16eaab3dfeeeffb7302730fa9284a
Pablo Expósito <161714534+Pabloexpo@users.noreply.github.com>
2025-02-12 18:02:24 +0100

    Add files via upload

--- proyectos.js
+++ proyectos.js
@@ -11,6 +11,9 @@ fetch(url)
 //Seleccionamos el select y le hacemos el evento
 let select = document.querySelector('#select'); 
 select.addEventListener('change', filtrar); 
+//Seleccionamos el buton para buscar y volvemos a filtrar
+let btnBuscar= document.querySelector('.buscar'); 
+btnBuscar.addEventListener('click', buscar); 
 function mostrarDatos(datos){
     console.log(datos)
     //Para cada repositorio, generaremos un article que se incluirá luego al section  
@@ -23,7 +26,7 @@ function mostrarDatos(datos){
                 <div class="card h-100 shadow">
                     <div class="card-body">
                         <h5 class="card-title">${dato.name}</h5>
-                        <p class="card-text">${dato.description || "Sin descripción"}</p>
+                        <p class="">${dato.description || "Sin descripción"}</p>
                         <a href="${dato.html_url}" target="_blank" class="btn btn-primary">Ver Proyecto</a>
                     </div>
                 </div>
@@ -33,7 +36,18 @@ function mostrarDatos(datos){
         section.innerHTML += `</div>`; // Cerrar la fila
     });
 }
-
+function buscar(e){
+    let input = document.querySelector('#input'); 
+    let valor = input.value; 
+    //console.log(valor)
+    let busqueda = datosTotales.filter (dato => {
+        if (dato.name.includes(valor)){
+            return dato.name
+        } 
+    })
+    mostrarDatos(busqueda)
+    input.value=''; 
+}
 function filtrar(e){
     let valor = e.target.value; 
     console.log(valor)
